4c3d4cdf5add555d55ec18c8be1f1d1ca5e9af3b
odan <odan3240@gmail.com>
2024-06-30 02:26:26 +0900

    add commet

--- src/repro.tsx
+++ src/repro.tsx
@@ -9,4 +9,13 @@ const Link = (_: { href: string | UrlObject }) => null;
 const X1 = <Button href=""></Button>;
 
 const href = {} as UrlObject;
-const X2 = <Button component={Link} href={href}></Button>;
+const X2 = <Button component={Link} href={href}></Button>; // Occurs type error
+/*
+No overload matches this call.
+  Overload 1 of 3, '(props: { href: string; } & ButtonOwnProps & Omit<ButtonBaseOwnProps, "classes"> & CommonProps & Omit<Omit<DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>, "ref"> & { ...; }, "className" | ... 24 more ... | "variant">): Element', gave the following error.
+    Type 'UrlObject' is not assignable to type 'string'.
+  Overload 2 of 3, '(props: { component: (_: { href: string | UrlObject; }) => null; } & ButtonOwnProps & Omit<ButtonBaseOwnProps, "classes"> & CommonProps & Omit<...>): Element | null', gave the following error.
+    Type 'UrlObject' is not assignable to type 'string'.
+  Overload 3 of 3, '(props: DefaultComponentProps<ExtendButtonBaseTypeMap<ButtonTypeMap<{}, "button">>>): Element | null', gave the following error.
+    Type 'UrlObject' is not assignable to type 'string'.
+*/
